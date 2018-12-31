5d38cae34c938944f57d24536f0d38eea8ad90b1
Chambers <chamjr@DRBA.NET>
2018-12-31 10:32:43 -0500

    Complete 6.6

---
+++ Chp_6/6.6.py
@@ -0,0 +1,31 @@
+"""Use the drawSquare function to print 5 pink squares seperate from each other.
+Assume each side is 20 units."""
+
+import turtle
+
+
+def drawSquare(t, sz):
+    """Get turtle t to draw a square of sz side"""
+
+    for i in range(4):
+        t.forward(sz)
+        t.left(90)
+
+
+def main():
+    wn = turtle.Screen()  # Set up the window
+    wn.bgcolor("lightgreen")
+
+    alex = turtle.Turtle()  # create alex
+    alex.color("pink")
+
+    for i in range(5):
+        drawSquare(alex, 20)
+        alex.penup()
+        alex.forward(50)
+        alex.pendown()
+
+    wn.exitonclick()
+
+
+main()
